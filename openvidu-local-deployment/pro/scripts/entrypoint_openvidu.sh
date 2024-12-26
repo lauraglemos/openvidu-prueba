4bda61994a31dabae6fd25fe3d106e5b9d3af183
Laura González Lemos <lauraglemossarria@gmail.com>
2024-12-26 18:31:41 +0100

    docker

---
+++ openvidu-local-deployment/pro/scripts/entrypoint_openvidu.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+if [ "$LAN_PRIVATE_IP" != "none" ]; then
+    export NODE_IP="$LAN_PRIVATE_IP"
+fi
+
+# Configure container private IP as node private IP
+LIVEKIT_OPENVIDU_NODE_PRIVATE_IP="$(hostname -i)"
+export LIVEKIT_OPENVIDU_NODE_PRIVATE_IP
+
+./livekit-server "$@"
